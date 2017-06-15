e4cc3fc1e1e5432a9ca0d7747602122389ef8549
IzzyRou <i.routledge15@imperial.ac.uk>
2017-06-15 10:47:48 +0100

    added functions

--- UsefulFunctions/R/UsefulFunctions.R
+++ UsefulFunctions/R/UsefulFunctions.R
@@ -1,11 +1,4 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
+
 #
 # Some useful keyboard shortcuts for package authoring:
 #
@@ -54,3 +47,93 @@ bootstrap<-function(x,n,nboot=100000){
 
   return(cat("Confidence interval: ",ci, "\n"))
 }
+
+### incidence per group per day
+daily.incidence.group<-function(obs.date,obs.group){
+  group<-obs.group
+  date.range<-c(min(obs.date),max(obs.date))
+  tt<-table(obs.date,obs.group)
+  Groups<-colnames(tt)
+  Dates<-(rownames(tt))
+  date<-seq(date.range[1],date.range[2],1)
+  f<-which(as.character(date) %in% Dates)
+  incidence<-data.frame(date)
+  for (i in 1:ncol(tt)){
+    x<-rep(0,length(date))
+    x[f]<-tt[,i]
+    incidence<-cbind(incidence,x)
+    names(incidence)[i+1]<-Groups[i]
+  }
+  return (incidence)
+}
+
+###### Force of Infection
+daily.foi.group<-function(incidence,n,t,SI){
+  foi<-incidence
+  foi[,2:n]<-0
+  WS<-rev(SI)%*%matrix(1,1,n)
+  if (n>1){
+    for (i in 1:t){
+      f=max(c(1,(i-SItrunc)))
+      foi[i,2:n]=colSums(incidence[f:i,2:n]*WS[((SItrunc+1)-(i-f)):(SItrunc+1),])
+    }
+  }else{
+    for (i in 1:t){
+      f=max(c(1,(i-SItrunc)))
+      foi[i,2:n]=sum(incidence[f:i,2:n]*WS[((SItrunc+1)-(i-f)):(SItrunc+1),])
+    }
+  }
+  return (foi)
+}
+
+#### discrete SI
+DiscretizeSI <- function(mu,CV,SItrunc){
+  SIdistr <- sapply(0:SItrunc, function(k) DiscrSI(k, mu, CV*mu))
+  SIdistr <- SIdistr/sum(SIdistr)
+  return(SIdistr)
+}
+
+#####continuous colour scale for base plot
+legend.col <- function(col, lev){
+
+  opar <- par
+
+  n <- length(col)
+
+  bx <- par("usr")
+
+  box.cx <- c(bx[2] + (bx[2] - bx[1]) / 1000,
+              bx[2] + (bx[2] - bx[1]) / 1000 + (bx[2] - bx[1]) / 50)
+  box.cy <- c(bx[3], bx[3])
+  box.sy <- (bx[4] - bx[3]) / n
+
+  xx <- rep(box.cx, each = 2)
+
+  par(xpd = TRUE)
+  for(i in 1:n){
+
+    yy <- c(box.cy[1] + (box.sy * (i - 1)),
+            box.cy[1] + (box.sy * (i)),
+            box.cy[1] + (box.sy * (i)),
+            box.cy[1] + (box.sy * (i - 1)))
+    polygon(xx, yy, col = col[i], border = col[i])
+
+  }
+  par(new = TRUE)
+  plot(0, 0, type = "n",
+       ylim = c(min(lev), max(lev)),
+       yaxt = "n", ylab = "",
+       xaxt = "n", xlab = "",
+       frame.plot = FALSE)
+  axis(side = 4, las = 2, tick = FALSE, line = .25)
+  par <- opar
+}
+
+#par(mar=c(5.1,4.1,4.1,2.1)+1)
+#par(mfrow=c(1,2))
+#legend.col(colfunc(1000),likelihood[!wh])
+#legend.col(colfunc(1000),A[!wh])
+
+#colfunc <- colorRampPalette(c("snow1","snow2","snow3","seagreen","orange","firebrick","red"), space = "rgb",bias=1)#colors
+#colfunc <- colorRampPalette(c('yellow','orange','red','brown'),bias=1)
+
